12401fa2bc58980b794c68d4f05d09889cbd1598
Q <libnexus@outlook.com>
2024-03-26 17:17:50 +0000

    Add new files

--- src/main/java/com/team6/academigymraeg/controller/NounController.java
+++ src/main/java/com/team6/academigymraeg/controller/NounController.java
@@ -4,6 +4,7 @@ import com.team6.academigymraeg.model.Gender;
 import com.team6.academigymraeg.model.Noun;
 import com.team6.academigymraeg.repo.NounRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
+
 @Controller
 public class NounController {
 
@@ -20,6 +22,7 @@ public class NounController {
     private NounRepository nounRepo;
 
     @GetMapping("/noun")
+    @Secured({"ADMIN","INSTRUCTOR"})
     public String noun(Model m) {
         if (!m.containsAttribute("nouns")) {
             m.addAttribute("nouns", nounRepo.findAll());
@@ -30,29 +33,51 @@ public class NounController {
     @RequestMapping(value = "/noun", method = RequestMethod.GET, params = "edit")
     public String edit(Model m, @RequestParam(value = "edit", required = false) Integer nounID) {
         Optional<Noun> changeNoun = nounRepo.findById(nounID);
+        Noun noun = changeNoun.get();
+        String english = noun.getEnglish();
+        String welsh = noun.getWelsh();
+        Gender gender = noun.getGender();
+        return editNoun(m, nounID, english, welsh, gender);
+    }
 
-        return "noun";
+    @GetMapping("/nounEdit")
+    public String editNoun(Model m, Integer nounID,
+                           @RequestParam(value = "english", required = false) String english,
+                           @RequestParam(value = "welsh", required = false) String welsh,
+                           @RequestParam(value = "gender", required = false) Gender gender) {
+
+        Optional<Noun> changeNoun = nounRepo.findById(nounID);
+
+        if (changeNoun.isPresent()){
+            Noun noun = changeNoun.get();
+
+            noun.setEnglish(english);
+            noun.setWelsh(welsh);
+            noun.setGender(gender);
+            nounRepo.save(noun);
+        }
+
+        return noun(m);
     }
 
 
     @RequestMapping(value = "/noun", method = RequestMethod.GET, params = "delete")
     public String delete(Model m, @RequestParam(value = "delete", required = false) Integer nounID) {
         Optional<Noun> changeNoun = nounRepo.findById(nounID);
-        if (!changeNoun.isEmpty()){
+        if (changeNoun.isPresent()){
             nounRepo.deleteById(nounID);
         }
         return noun(m);
     }
 
 
-    @RequestMapping(value = "/add-noun", method = RequestMethod.GET, params = { "add" } )
+    @RequestMapping(value = "/noun", method = RequestMethod.GET, params = {"add"} )
     public String add(Model m,
-                      @RequestParam(value = "nounID", required = false) Integer nounID,
                       @RequestParam(value = "english", required = false) String english,
                       @RequestParam(value = "welsh", required = false) String welsh,
-                      @RequestParam(value = "gender", required = false) String gender) {
-        nounRepo.save(new Noun(-2, english, welsh, Gender.FEMALE));
+                      @RequestParam(value = "gender", required = false) Gender gender) {
+        nounRepo.save(new Noun(english, welsh, gender));
 
-        return "noun";
+        return noun(m);
     }
 }
